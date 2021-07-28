5be44b77cc75f06d8ff22699441ee5ac7b3aed91
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-28 18:38:01 -0300

    Subscription y unsubscribe

--- curso-rxjs/src/index.ts
+++ curso-rxjs/src/index.ts
@@ -1,7 +1,40 @@
 import { Observable, Observer } from 'rxjs';
 
 const observer: Observer<any> = {
-     next: value => console.log('siguiente [next]:', value),
-     error: error => console.error('error [obs]', error),
-     complete: () => console.info('completado [obs]')
+     next: value => console.log('siguiente:', value),
+     error: error => console.warn('error:', error),
+     complete: () => console.info('completado')
 };
+
+const intervalo$ = new Observable( subscriber => {
+
+     // Crear un contador, 1, 2, 3, 4, 5,...
+     let count = 0;
+
+     const interval = setInterval( () => {
+          // cada segundo
+          count++;
+          subscriber.next( count );
+          console.log(count);
+
+     }, 1000);
+
+     return () => {
+          clearInterval(interval);
+          console.log('Intervalo destruido');
+     }
+
+});
+
+const subs1 = intervalo$.subscribe();
+const subs2 = intervalo$.subscribe();
+const subs3 = intervalo$.subscribe();
+
+setTimeout(() => {
+     subs1.unsubscribe()
+     subs2.unsubscribe()
+     subs3.unsubscribe()
+
+     console.log('Completado timeout');
+     
+}, 3000);
